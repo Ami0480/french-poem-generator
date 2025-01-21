dd0f856b79d87270c7579ca433fcbcc6b67dcbe8
Ami0480 <amifuku80@gmail.com>
2025-01-21 13:46:00 +0800

    added AI API

--- js/index.js
+++ js/index.js
@@ -1,12 +1,23 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "The poem will be here",
+    strings: response.data.answer,
     autoStart: true,
     delay: 10,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let userElement = document.querySelector("#user-instructions").value;
+  let apiKey = "25ba4b8ct7fc123o0c3d6fccfc118bbd";
+  let prompt = `generate a very short french poem about ${userElement}.`;
+  let context =
+    "You are a smart poem expert and write a poem. Your mission is generate 4 short poems and write separate line with <br />. Write SheCodes AI inside <strong></strong> at the bottom";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
